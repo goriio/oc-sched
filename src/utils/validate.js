0e1e3c45055759c62aff273e996f4c274ebccaef
goriio <gdsuaperoii@iskolarngbayan.pup.edu.ph>
2022-05-24 18:36:21 +0800

    update url validation

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,5 +1,3 @@
 export function isUrl(url) {
-  return /((?:(?:http?|ftp)[s]*:\/\/)?[a-z0-9-%\/\&=?\.]+\.[a-z]{2,4}\/?([^\s<>\#%"\,\{\}\\|\\\^\[\]`]+)?)/.test(
-    url
-  );
+  return /^(ftp|http|https):\/\/[^ "]+$/.test(url);
 }
